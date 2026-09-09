8c7522f8b1556bc27f0bccd13d69d6e1c7cf67a4
gseoin17-dotcom <gseoin17@gmail.com>
2026-09-09 18:45:33 +0900

    Update jionm.py

--- jionm.py
+++ jionm.py
@@ -1445,16 +1445,13 @@ with left_col:
 
         if not st.session_state.is_rebirth:
             warp_prices = {
-                10: 20000000,
-                15: 100000000,
-                20: 400000000,
-                25: 2000000000,
-                30: 10000000000,
+                w_level: SMELL_DB[False][w_level]["price"] * 2
+                for w_level in [10, 15, 20, 25, 30]
             }
             active_warps = warp_prices.items()
         else:
             season2_warp_prices = {
-                w_level: int(SMELL_DB[True][w_level]["price"] / 2)
+                w_level: SMELL_DB[True][w_level]["price"] * 2
                 for w_level in [5, 10, 15, 20]
             }
             active_warps = season2_warp_prices.items()
